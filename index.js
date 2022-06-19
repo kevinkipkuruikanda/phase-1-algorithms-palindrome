e9cb1b81e34f2f99aaa05c67642793ae93ea17ab
kevin@kevin <kevin1kipkurui@gmail.com>
2022-06-19 22:53:55 +0300

    commit tree b6dbb3649332325e8c03102da3e6e61c15c21181

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // converts every non alpahnumeric characters
+  const re=/[\W_]/g;
+
+
+// LOWWord converts any string passsed to lowercase and any non non alpahnumeric function is replaced with no space
+  const lowWord=word.toLowerCase().replace(re,"");
+  // checks the reverse of word is after spliting each character and later join 
+  const reverseWord=lowWord.split("").reverse().join("");
+  if(reverseWord===lowWord){
+   return true;
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
